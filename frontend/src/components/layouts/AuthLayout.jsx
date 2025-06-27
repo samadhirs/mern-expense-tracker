d120a9d3b160ef9e3258c7ff9e51176b3978e9c4
Samadhi Rasanjalee <126862124+samadhirs@users.noreply.github.com>
2025-06-27 18:05:48 +0530

    frontend: update AuthLayout.jsx

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -8,6 +8,18 @@ const AuthLayout = ({children}) => {
             <h2 className="text-lg font-medium text-black">Expense Tracker</h2>
             {children}
         </div>
+
+        <div className='hidden md: block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'>
+          <div className='w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5' />
+          <div className='w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] right-[10%]' />
+          <div className='w-48 h-56 rounded-[40px] bg-violet-500 absolute -top-7 -left-5' />
+
+          <img 
+            src="" 
+            alt="" 
+            className=''
+          />
+        </div>
     </div>
   )
 }
