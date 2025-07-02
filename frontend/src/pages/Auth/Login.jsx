0f36d2a9a22f58466e686c3cbd8d537c48fb7c3d
Samadhi Rasanjalee <126862124+samadhirs@users.noreply.github.com>
2025-07-02 21:23:25 +0530

    frontend: Input folder created

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
+import {useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const[email,setEmail] = useState("");
+  const[password,setPassword] = useState("");
+  const[error,setError] = useState(null);
+
+  const navigate=useNavigate();
+
+  //Handle Login Form Submit
+  const handleLogin = async (e) => {}
+
+
+
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
@@ -11,6 +23,16 @@ const Login = () => {
         <p className="text-xs text-slate-700 mt-[5px] mb-6 ">
           Please enter your  details to log in
         </p>
+
+        <form onSubmit={handleLogin}>
+          <input
+            value={email}
+            onChange={({target}) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+            />
+          </form>
       </div>
     </AuthLayout>
   )
