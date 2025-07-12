4d39616100167701f3aef3fded648700689f47ed
Samadhi Rasanjalee <126862124+samadhirs@users.noreply.github.com>
2025-07-12 10:54:56 +0530

    backend: .env database setup done

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
-const express = require("cors");
+const express = require("express");
+const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
 //middleware to handle CORS
-app.arguments(
-    CORS({
+app.use(
+    cors({
         origin: process.env.CLIENT_URL || "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
@@ -15,6 +17,8 @@ app.arguments(
 );
 
 app.use(express.json());
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
