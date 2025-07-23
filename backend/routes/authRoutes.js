450666dedc881be3c0a8e85d22aa921d11f3f4d1
Samadhi Rasanjalee <126862124+samadhirs@users.noreply.github.com>
2025-07-24 00:50:29 +0530

    backend: uploadMiddleware.js updated

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
+const upload = require("../middleware/uploadMiddleware"); // 👈 ADD THIS
+
 
 const{
     registerUser,
@@ -14,6 +16,16 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 router.get("/getUser", protect, getUserInfo);
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "no file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+     req.file.filename
+    }`;
+    res.status(200).json({ imageUrl });
+});
 
 module.exports = router;
 
--- backend/server.js
+++ backend/server.js
@@ -26,5 +26,8 @@ connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 
+//Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
