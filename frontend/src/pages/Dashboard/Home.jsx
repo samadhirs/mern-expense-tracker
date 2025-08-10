5ddf53edb104439f99fd0505fa719cd44e2a167c
Samadhi Rasanjalee <126862124+samadhirs@users.noreply.github.com>
2025-08-10 20:13:19 +0530

    frontend: Home.jsx updated

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="my-5 mx-auto"></div>
+
+    </DashboardLayout>
+  );
+};
 
 export default Home;
