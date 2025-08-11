d6aba1e0e9808ab536698eae1ea55b1b0448d917
Samadhi Rasanjalee <rssamadhi062@gmail.com>
2025-08-12 01:21:45 +0530

    frontend: homepage info cards created

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -1,14 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
 import { useUserAuth } from '../../hooks/useUserAuth';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import InfoCard from '../../components/Cards/InfoCard';
+
+// Icons
+import { IoMdCard } from 'react-icons/io';
+import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
+import { addThousandSeparator } from '../../utils/helper';
 
 const Home = () => {
   useUserAuth();
+
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if (loading) return;
+    setLoading(true);
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } 
+    catch (error) {
+      console.log('Error fetching dashboard data:', error);
+    } 
+    finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  }, []);
   
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">
-        Home
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance"
+            value={addThousandSeparator(dashboardData?.totalBalance || 0)}
+            color="bg-blue-500 text-white"
+          />
+
+          <InfoCard
+            icon={<LuWalletMinimal />}
+            label="Total Income"
+            value={addThousandSeparator(dashboardData?.totalIncome || 0)}
+            color="bg-orange-500 text-white"
+          />
+
+          <InfoCard
+            icon={<LuHandCoins />}
+            label="Total Expense"
+            value={addThousandSeparator(dashboardData?.totalExpense || 0)}
+            color="bg-red-500 text-white"
+          />
+        </div>
       </div>
     </DashboardLayout>
   )
