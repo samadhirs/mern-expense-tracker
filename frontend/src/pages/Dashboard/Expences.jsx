1572b8499c6604fbda80869de8eaaf014b5ca853
Samadhi Rasanjalee <rssamadhi062@gmail.com>
2025-08-13 01:19:58 +0530

    frontend: overall expense done

--- frontend/src/pages/Dashboard/Expences.jsx
+++ frontend/src/pages/Dashboard/Expences.jsx
@@ -1,8 +1,112 @@
-//import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useUserAuth } from '../../hooks/useUserAuth';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import ExpenseOverview from '../../components/ExpenseComponents/ExpenseOverview';
 
 const Expences = () => {
+  useUserAuth();
+
+  const [expenseData, setExpenseData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlart, setOpenDeleteAlert] = useState({
+    show: false,
+    data: null,
+  });
+  const [openAddExpenseModal, setOpenAddExpenseModal] = useState(false);
+
+  // Get expense data
+  const fetchExpenseDetails = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.EXPENSE.GET_ALL_EXPENSE}`
+      );
+
+      if (response.data) {
+        setExpenseData(response.data);
+      }
+    } 
+    catch (error) {
+      console.log("Error fetching income data:", error);
+    } 
+    finally {
+      setLoading(false);
+    }
+  };
+
+  // Add expense handler
+  const handleAddExpense = async (income) => {
+    const { category, amount, date, icon } = expense;
+
+    if (!category.trim()) {
+      toast.error("Please enter expense category.");
+      return;
+    }
+
+    if (!amount || isNaN(amount) || Number(amount) <= 0) {
+      toast.error("Please enter a valid amount");
+      return;
+    }
+
+    if (!date) {
+      toast.error("Please select a date");
+      return;
+    }
+
+    try {
+      await axiosInstance.post(API_PATHS.EXPENSE.ADD_EXPENSE, {
+        category,
+        amount,
+        date,
+        icon,
+      });
+
+      setOpenAddExpenseModal(false);
+      toast.success("Expense added successfully");
+      fetchIncomeDetails();
+    } 
+    catch (error) {
+      console.error(
+        "Error adding expense:",
+        error.response?.data?.message || error.message
+      );
+    }
+  };
+
+  useEffect(() => {
+    fetchExpenseDetails();
+
+    return () => {};
+  }, []);
+
   return (
-    <div>Expences</div>
+    <DashboardLayout activeMenu="Expense">
+      <div className='my-5 mx-auto'>
+        <div className='grid grid-cols-1 gap-6'>
+          <div className=''>
+            <ExpenseOverview
+              transactions={expenseData}
+              onExpenseIncome={() => setOpenAddExpenseModal(true)}
+            />
+          </div>
+        </div>
+
+        <Modal
+          isOpen={openAddExpenseModal}
+          onClose={() => setOpenAddExpenseModal(false)}
+          title="Add Expense"
+        >
+          <AddExpenseForm
+            onAddExpense={handleAddExpense}
+          />
+        </Modal>
+      </div>
+    </DashboardLayout>
   )
 }
 
